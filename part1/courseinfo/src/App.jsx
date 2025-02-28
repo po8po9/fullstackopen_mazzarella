da427109321539b71b1902edfe8c0018c740afbf
po8po9 <faustomazzarella@gmail.com>
2025-02-28 08:26:07 +0100

    part 1.3 - object variables

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,11 +1,22 @@
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+
+    //Part 1.3  - new definitions of variables, refactor so app still works
+    
+ const course = 'Half Stack application development'
+
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
+    
 
   const Header = (course)=>{
 	return (
@@ -13,10 +24,11 @@ const App = () => {
 	);
   }
 
-  const Part = (course) => {
+    const Part = (part) => {
+	console.log (part);	
 	    return(
 	    <p>
-		{course.part} {course.exercises}
+		{part.name} {part.exer}
 	    </p>
 		);
 	}
@@ -26,19 +38,19 @@ const App = () => {
 	
 	return(
 	<>   
-	    <Part part={part1} exercises={exercises1}/>
-	    <Part part={part2} exercises={exercises2}/>
-	    <Part part={part3} exercises={exercises3}/>
+	    <Part  name={part1.name} exer={part1.exercises}/>
+	    <Part  name={part2.name} exer={part2.exercises}/>
+	    <Part  name={part3.name} exer={part3.exercises}/>
 	</> 
 	);
 
 	};
 
     const Total = () => {
-
+	//here the sum is not pretty, but it works
 	return (
 	    <p>
-		Number of exercises = {exercises1+exercises2+exercises3}
+		Number of exercises = {part1.exercises+part2.exercises+part3.exercises}
 	    </p>
 
 	)
