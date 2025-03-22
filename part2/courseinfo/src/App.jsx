d4f7ba73b655a1b86124cdbbacfc37ed77d93186
po8po9 <faustomazzarella@gmail.com>
2025-03-22 21:29:51 +0100

    exercise 2.5 but with random IDs, not convenient

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -2,53 +2,75 @@
 import Course from './Components'
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
-    ]
-  }
-  const course2 = {
-    id: 2,
-    name: 'Full Stack application development',
-    parts: [
-      {
-        name: 'inner of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to rock data',
-        exercises: 123,
-        id: 2
-      },
-      {
-        name: 'State of a world',
-        exercises: 14,
-        id: 3
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        },
+        
+      ]
+    },
+    {
+      name: 'C and Linux',
+      id: 2,
+      parts: [
+        {
+          name: 'Processes',
+          exercises: 356,
+          id: 1
+        },
+        {
+          name: 'Headaches',
+          exercises: 57,
+          id: 2
+        },
+        
+      ]
+    }
+  ]
+
 
   return (
     <div>
-  <Course course={course} />
-  <Course course={course2} />
+  <Course courses={courses} />
+
+  
 
 
   </div>
--- part2/courseinfo/src/Components.jsx
+++ part2/courseinfo/src/Components.jsx
@@ -1,21 +1,13 @@
-const Course = ({course}) => {
-  //console.log (course.parts)
-          return (
-          <div>
-            <Header title = {course.name} />
-            <Content parts = {course.parts} />
-            <Total course_Parts={course.parts}/>
-          </div>
-  )
-}
-const Header = ({title}) =>{
 
+const Header = ({title}) =>{
+   
     return (<h1>{title}</h1>) 
   
   }
 
 const Content = ({parts}) =>{
- return(
+
+  return(
  <ul>
   {parts.map (x => 
   <Part key={x.id} course_part={x}/>)}
@@ -23,7 +15,7 @@ const Content = ({parts}) =>{
 )
 
 }
-
+//Part called by content
 const Part = ({course_part}) =>{
  return( 
     <li>
@@ -33,13 +25,55 @@ const Part = ({course_part}) =>{
 }
 const Total = ({course_Parts}) => {
   //let totalExercises = course_Parts.map (part => part.exercises)
-  console.log (course_Parts)
-  let totalEx = course_Parts.reduce((acumm, curr) => acumm + curr.exercises,0)
-  console.log(totalEx)
+  // console.log (course_Parts)
+  let totalEx = course_Parts.reduce((acumm, curr) => acumm + curr.exercises,0) //only sums .exercies
+  // console.log(totalEx)
     /*  totalExercises.forEach(element => {
         sum += element*/
 
 return <div>Total exercises {totalEx}</div>
 
 }
+
+const generateID = (originalID) => {
+  let usedIDs = []
+  let newID = Math.floor (Math.random() * 100 * originalID) //otherwise IDs are repeated in map function
+  usedIDs.push(newID)
+  if (newID in usedIDs){  
+    console.log ("used ID, recalculating")
+    generateID (Math.floor(Math.random()*100))
+}
+//console.log (newID)  
+//console.log (usedIDs)
+  
+  return newID
+}
+
+const Course = ({courses}) => {
+  
+
+ 
+  return (
+    
+      courses.map ( course=> {
+             
+        // console.log (course.id)
+        return (
+              <div key = {generateID(course.id)}>
+
+              <Header key = {generateID(course.id)} title = {course.name} />
+              <Content key = {generateID(course.id)} parts = {course.parts} />
+              <Total key = {generateID(course.id)} course_Parts={course.parts}/>
+        
+              </div>) 
+              }
+            )
+                       
+  
+)
+  }
+
+        
+          
+
 export default Course
