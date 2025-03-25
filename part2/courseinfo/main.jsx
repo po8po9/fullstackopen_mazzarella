bb555d7ce53a6f0e45f2b46b56ce582750516c18
po8po9 <faustomazzarella@gmail.com>
2025-03-25 08:48:46 +0100

    re-organization

---
+++ part2/courseinfo/main.jsx
@@ -0,0 +1,9 @@
+import ReactDOM from 'react-dom/client'
+
+
+import App from './App'
+import Components from './Components'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <App  />
+)
