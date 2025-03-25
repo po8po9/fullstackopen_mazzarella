4250fe5c55293ceabf57d4b44cf74e6af0871e10
po8po9 <faustomazzarella@gmail.com>
2025-03-25 08:54:32 +0100

    reorganization

--- part2/courseinfo/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import ReactDOM from 'react-dom/client'
-
-
-import App from './App'
-import Components from './Components'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <App  />
-)
