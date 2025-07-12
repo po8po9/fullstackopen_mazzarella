813165240efdc858881ad0b113ea0c230aa384e4
po8po9 <faustomazzarella@gmail.com>
2025-07-12 09:39:31 +0200

    Excercise 2.7

--- part2/phonebook_exercises/src/App.jsx
+++ part2/phonebook_exercises/src/App.jsx
@@ -1,9 +1,9 @@
 import { useState } from 'react'
 import Person from './components/Person'
-
+const error_Message= 'This person already exists.'
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Artos Hellas' }
   ]) 
   const [newName, setNewName] = useState('')
   
@@ -16,7 +16,18 @@ const App = () => {
 
   const addNewName = (ev) => {
     ev.preventDefault()
+    //2.7 -> look for new name in array persons
+    let name_inUse = persons.find ((ind)=> (ind.name === newName))
+    //catch, if not undefined then there is a matching object; after alerting set newName back to 0
+    if (name_inUse!= undefined) {
+      alert (error_Message)
+      setNewName('')
+      return}
+    console.log(name_inUse)
+    console.log(persons[0].name)
+
     //after preventing default -> add newName to object persons
+    //but first create a new object (do not pass newName directly)
     const newPerson={
       name:newName
     }
@@ -39,7 +50,7 @@ const App = () => {
       </form>
       <ul>
         {persons.map((individual)=>(
-          <Person ind={individual} key={crypto.randomUUID()} />
+          <Person ind={individual} key={individual.name} />
          ))
         }
 
