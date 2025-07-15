14d9a67ac5a1098a8795f6033fb971ebd0440a3f
po8po9 <faustomazzarella@gmail.com>
2025-07-15 09:34:02 +0200

    2.11 db.json

--- part2/phonebook_exercises/src/App.jsx
+++ part2/phonebook_exercises/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Persons from './components/Persons'
 import Form_Add from './components/Form_Add'
 /*****************CONSTANTS ****************/
@@ -10,18 +11,25 @@ const App = () => {
 
   const error_Message= ' already exists in the phonebook.'
 
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [newSearch, setNewSearch] = useState('')
   const [searchTrue, toggleSearch] = useState(false)
   const showContacts = searchTrue ? persons.filter(ind => ind.name.toLowerCase().includes(newSearch.toLocaleLowerCase())) : persons 
+
+//*************************GET DATA FROM JSON SERVER***************/
+
+
+  useEffect( () => {
+      console.log("this is effect")
+      axios.get('http://localhost:3001/persons').then (
+        (response) => {
+          console.log(response)
+          setPersons(response.data)
+        })
+      }, [] )
 
 
 
@@ -67,7 +75,7 @@ const App = () => {
       number:newNumber
     }
     /**Add */
-    console.log (newPerson)
+  
     setPersons(persons.concat(newPerson))
     /**Clear */
     setNewName('')
